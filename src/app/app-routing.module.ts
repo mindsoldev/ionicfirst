00b6ed7fe0ad7e81552ba87ee2c5e9338a0c6432
Harsán Péter <hapegabor@gmail.com>
2023-12-07 17:43:34 +0100

    Remove hone page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '',
     pathMatch: 'full'
   },
-];
+ ;
 
 @NgModule({
   imports: [
