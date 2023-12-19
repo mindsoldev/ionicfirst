1daec778dc2e395ed97376c535ed59ffe9fa81a8
Harsán Péter <hapegabor@gmail.com>
2023-12-19 10:25:22 +0100

    Sign up page designed

--- src/app/auth-screens/auth-screen/sign-up/sign-up.component.ts
+++ src/app/auth-screens/auth-screen/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,8 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent  implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+  type: boolean = true;
 
-  ngOnInit() {}
+  constructor() { 
+    this.initForm();
+  }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = new FormGroup({
+      username: new FormControl(null, {validators: [Validators.required]}),
+      email: new FormControl(null, {validators: [Validators.required]}),
+      password: new FormControl(null, {validators: [Validators.required, Validators.minLength(8)]})
+    })
+  }
+
+  changeType() {
+    this.type = !this.type;
+  }
+
+  onSubmit() {
+    if (!this.form.valid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    console.log(this.form.value);
+  }
 
 }
+
